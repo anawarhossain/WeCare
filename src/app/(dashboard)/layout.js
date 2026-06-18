516a822d28ff7a1f58f07f673f3b5d45708f90f8
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-06-19 01:36:49 +0600

    feat: add Dashboard page and layout component for dashboard structure

---
+++ src/app/(dashboard)/layout.js
@@ -0,0 +1,27 @@
+
+
+export default function DashboardLayout({ children }) {
+  return (
+    <>
+      {/* // show navbar in signup and signin page */}
+      {/* <Navbar/> */}
+
+      <div className="flex min-h-screen">
+        {/* ── Left Sidebar ── */}
+        {/* <DashboardSidebar /> */}
+
+        {/* ── Main content area ── */}
+        <div className="flex flex-col flex-1 min-w-0">
+          {/* ── Top bar ── */}
+          {/* <DashboardTopbar /> */}
+
+          {/* ── Page content ── */}
+          <main className="flex-1 overflow-auto p-6">{children}</main>
+        </div>
+      </div>
+
+      {/* //  show Footer in signup and signin page */}
+      {/* <Footer /> */}
+    </>
+  );
+}
