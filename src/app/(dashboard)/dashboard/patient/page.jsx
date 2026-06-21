ae67232b6242f330c103f7afb6c899c19526746f
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-06-21 11:02:56 +0600

    feat: add PatientDashboardPage and PatientProfilePage components

---
+++ src/app/(dashboard)/dashboard/patient/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PatientDashboardPage = () => {
+    return (
+        <div>
+            Patient Dashboard Page
+        </div>
+    );
+};
+
+export default PatientDashboardPage;
